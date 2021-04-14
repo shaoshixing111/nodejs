11df7556454d23f77f5e1afc32dd7a339b6897aa
shaoshixing <2931117362@qq.com>
2021-04-14 17:05:13 +0800

    mysql

---
+++ 4.14/mysql.js
@@ -0,0 +1,14 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'goods'
+});
+ 
+connection.connect();
+ 
+connection.query('select user_name,email from p_users limit 5', function (error, results, fields) {
+    console.log(results)
+  
+});
